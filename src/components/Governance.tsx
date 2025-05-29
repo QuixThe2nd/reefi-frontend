ea1a4b1a960d2142f10938a01e6375e09db85910
Parsa Yazdani (Quix) <parsa@yazdani.au>
2025-05-29 19:15:40 +1000

    Fix err

--- src/components/Governance.tsx
+++ src/components/Governance.tsx
@@ -3,7 +3,7 @@ import { type ReactElement } from 'react'
 export const Governance = (): ReactElement => {
   return <div className="bg-gray-800 p-4 rounded-xl border border-gray-700 mb-6">
     <h2 className="text-lg font-bold mb-2">Magpie Governance</h2>
-    <p className="text-gray-400 text-xs mt-2">Locked yMGP controls the entirety of Reefi&apos;s vlMGP voting power on Magpie votes. yMGP votes are cast to Reefi&apos;s DAO. Once Reefi voting is complete, the vote will be forwarded to Magpie.</p>
+    <p className="text-gray-400 text-xs mt-2">vMGP controls the entirety of Reefi&apos;s vlMGP voting power on Magpie votes. vMGP votes are cast to Reefi&apos;s DAO. Once Reefi voting is complete, the vote will be forwarded to Magpie.</p>
     <h2 className="text-lg font-bold mt-4 mb-2">Reefi Governance & Treasury</h2>
     <p className="text-gray-400 text-xs mt-2">
       Locked yMGP holders are able to vote on Reefi DAO proposals. The treasury receives 4% of compounded rMGP yield and 5% of rMGP withdrawals.
